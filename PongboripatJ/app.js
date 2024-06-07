8cc2bc19c01deba29345850c035c3b4ad92e3d09
ppbrp <pongboripatnioz@gmail.com>
2024-06-07 20:20:40 +0700

    First day1 (free time hobby)
    create landing page only

---
+++ PongboripatJ/app.js
@@ -0,0 +1,15 @@
+/*
+document.write("Hello World!");
+alert("Hello World!");
+console.log("hello JavaScript");
+console.error("hello JavaScript");
+*/
+
+let money,a,b;
+const loan = 100;
+a = 10;
+b =10;
+
+
+console.log(money);
+
